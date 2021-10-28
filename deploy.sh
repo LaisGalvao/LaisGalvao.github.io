19a7a16cb38b557675afae417395e61f6a7293c3
Laís <laisgbueno62@gmail.com>
2021-10-28 17:52:57 -0300

    deploy atualizado

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:LaisGalvao/LaisGalvao.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<https://github.com/LaisGalvao/LaisGalvao.github.io.git main:master
+#git push -f git@github.com:https://github.com/LaisGalvao/LaisGalvao.github.io.git master:master
 
 cd -
