b9a4c0e8f465eb64a432cb3bc8c20bc66cdc7769
Laís Galvão <laisgbueno62@gmail.com>
2022-07-26 12:46:58 -0300

    script e ediçaõ de arquivo

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+#git init
+git add .
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/LaisGalvao/LaisGalvao.github.io.git master:master
+git push -f git@github.com:LaisGalvao/LaisGalvao.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:https://github.com/LaisGalvao/LaisGalvao.github.io.git master:master
