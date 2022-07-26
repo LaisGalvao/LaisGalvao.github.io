1d7c866754fd78b80b06ec86d4b4fdc6151bc4ce
Laís Galvão <laisgbueno62@gmail.com>
2022-07-26 15:27:05 -0300

    💄feat: Navbar e footer usando tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import NavbarComponent from "./layout/NavbarComponent.vue";
+import FooterComponent from "./layout/FooterComponent.vue";
 </script>
 
 <template>
-  <div class="flex">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div>
+    <NavbarComponent />
+    <div class="flex">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue (edit edit edit edit edita 🤬)" />
+    <FooterComponent/>
   </div>
-  <HelloWorld msg="Vite + Vue (edit edit edit edit edita 🤬)" />
 </template>
 
 <style scoped>
