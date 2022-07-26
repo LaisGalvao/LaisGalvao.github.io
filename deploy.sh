d582ba964c8409e2dd95d677bc35a788b28073ac
Laís Galvão <laisgbueno62@gmail.com>
2022-07-26 14:39:08 -0300

    ultimo teste dessa bagaça de deploy 😡😠🤬

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,11 @@ cd dist/
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+git add .
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/LaisGalvao/LaisGalvao.github.io.git master
+git push -f origin master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:https://github.com/LaisGalvao/LaisGalvao.github.io.git master:master
