a9e8ddd50f07e4e7337439edc474945ffce2d5d7
Laís Galvão <laisgbueno62@gmail.com>
2022-07-26 14:32:09 -0300

    TEEEESSSTEEEE DROOOOGAAAAAAA

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ cd dist/
 # echo 'www.example.com' > CNAME
 
 git init
-git add .
+git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/LaisGalvao/LaisGalvao.github.io.git master
+git push -f origin master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:https://github.com/LaisGalvao/LaisGalvao.github.io.git master:master
