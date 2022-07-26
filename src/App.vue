9520e657e4b6c79b7d2bae96d4cfd836f77edc2b
Laís Galvão <laisgbueno62@gmail.com>
2022-07-26 13:04:16 -0300

    vamo testar essa bagaça

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue (edit)" />
+  <HelloWorld msg="Vite + Vue (edit edit edit)" />
 </template>
 
 <style scoped>
