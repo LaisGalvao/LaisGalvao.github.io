125feade3d02c28e675f81d1a794f64bb8d70a1c
Laís Galvão <laisgbueno62@gmail.com>
2024-03-05 13:20:46 +0000

    using vue cli

---
+++ portfolio/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" :style="{ background: theme.backgroundColor, color: theme.textColor }">
+    <NavbarComponent />
+    <MainContent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent.vue";
+import MainContent from "@/components/MainContent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  components: {
+    NavbarComponent,
+    MainContent,
+    FooterComponent,
+  },
+  data() {
+    return {
+      theme: {
+        backgroundColor: "#171717",
+        textColor: "#ffffff", // Cor do texto no tema escuro
+        primaryColor: "#7d14f5", // Cor primária
+      },
+    };
+  },
+};
+</script>
+
+<style>
+/* Adicione estilos globais aqui, se necessário */
+</style>
